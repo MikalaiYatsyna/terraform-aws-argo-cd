efa1326e0702e3668268d1514077ba9f1657af18
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-14 19:37:16 +0100

    Pin versions (#3)

---
+++ terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.7.5"
+  required_providers {
+    aws = {
+      version = "4.57.1"
+      source  = "hashicorp/aws"
+    }
+    kubernetes = {
+      version = "2.19.0"
+      source  = "hashicorp/kubernetes"
+    }
+    helm = {
+      version = "2.9.0"
+      source  = "hashicorp/helm"
+    }
+    tls = {
+      version = "4.0.4"
+      source  = "hashicorp/tls"
+    }
+  }
+}
