6090c4ab4664c456bf4c6c3553437879280fc359
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-14 20:02:32 +0100

    pin providers version
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- terraform.tf
+++ terraform.tf
@@ -1,21 +1,9 @@
 terraform {
-  required_version = "1.7.5"
+  required_version = "1.7.0"
   required_providers {
     aws = {
-      version = "4.57.1"
+      version = "5.40.0"
       source  = "hashicorp/aws"
-    }
-    kubernetes = {
-      version = "2.19.0"
-      source = "hashicorp/kubernetes"
-    }
-    helm = {
-      version = "2.9.0"
-      source = "hashicorp/helm"
-    }
-    tls = {
-      version = "4.0.4"
-      source = "hashicorp/tls"
     }
   }
 }
